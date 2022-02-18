2a00dcd8e2bab4cbff1b1272be1b7aeacb9ac24e
Undine Olina <97155884+undineolina@users.noreply.github.com>
2022-02-18 10:44:46 +0000

    ...

--- jaut.py
+++ jaut.py
@@ -1,33 +1,35 @@
-import laiks
-import cenas
-from datetime import datetime
-dienas=[]
-def funkcija(izvele):
-    if izvele == "A":
-      skaits=0
-      file = open("rezervacijas.txt")
-      lines = file.readlines()[1:]
-      file.close()
-      for line in lines:
-          skaits += 1
+import laiks #Iportē lietotāja definētu funkciju.
+import cenas #Iportē lietotāja definētu funkciju.
+from datetime import datetime # Importē datumus un laikus no funkcijas "datetime".
+dienas=[] # Izveido tikšu listu.
+numurs=0 # Piešķir mainīgajam vērtību 0.
+def funkcija(izvele): # izveido funkciju un piešķir tai vērtību.
+    if izvele == "A": # Pārbauda vai "izvele" ir vienādā ar "A", ja ir tad zemākās darbības izvadīs konsolē, to ko lietotājs ir izvēlējies.
+      skaits=0 # Piešķir mainīgajam vērtību 0.
+      file = open("rezervacijas.txt") # Atver failu.
+      lines = file.readlines()[1:] # Nolasa visas līnijas failā.
+      file.close() # Aizveras fails.
+      for line in lines: # Nolasa līniju skaitu.
+          skaits += 1 #pievieno mainīgajam vērtību attiecībā uz līniju skaitu.
 
-      if skaits == 0:
-          print("Nav neviena pieraksta!!")
-          print()
-      else:
+      if skaits == 0: # Pārbauda vai līniju skaits ir 0, ja nulle tad izvada teksu kas ir redzams zemāk.
+          print("Nav neviena pieraksta!!") 
+          print() 
+      else: # Parbauda vai līniju skaits nav nulle, ja nav tad nolasa un izvada tekstu un no faila nolasītās līnijas.
           file = open("rezervacijas.txt", "r")
+          print("Apskati jau rzervētos laikus un veic pierakstu sev izvedīgā laikā.")
           print(file.read())
-          file.close()  
+          file.close()  # Aizver failu.
 
 
     elif izvele == "B":
       with open("rezervacijas.txt", "r") as file:
           for pedeja_l in file:
             pass
-          if pedeja_l[0] == "#":
-            numurs = 1
-          else:
-            numurs = int(pedeja_l[0]) + 1
+      if pedeja_l[0] == "#":
+          numurs = 1
+      else:
+              numurs = int(pedeja_l[0]) + 1
       vards = input("Ievadi vārdu un uzvārdu: ")
       print()
       datums = input("Ievadi datumu (ievadi-gadu.mēnesi.dienu-(Pimēram:2022.02.16)): ")
@@ -71,10 +73,10 @@ def funkcija(izvele):
               frizieris="Sencis"
             else:
               frizieris1="Pēteris"
-              file=open("rezervacijas.txt","a")                
-              file.write(f"{numurs}\t\t\t{vards}\t\t\t{datums}\t\t\t{p_laiks}\t\t\t{t_nr}\t\t\t{frizieris1}\n") 
-              file.close()
-              print("Pieraksts ir veikts! :)")
+            file=open("rezervacijas.txt","a")                
+            file.write(f"{numurs}\t\t\t{vards}\t\t\t{datums}\t\t\t{p_laiks}\t\t\t{t_nr}\t\t\t{frizieris1}\n") 
+            file.close()
+            print(f"Pieraksts ir veikts, gaidīsim jūs {datums} plkst. {p_laiks}! :)")
                 
 
     elif izvele == "C":
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import jaut
+import jaut   #Iportē lietotāja definētu funkciju.
+
+# Konsolē izvada salona nosaukumu un, kādas darbības programma piedāvā.
 print("VEIC PIERAKSTU SALONĀ 'PĒTERIS' ")
 print("Ja nevēlaties izmantot programmu, lai pierakstītos zvaniet uz +371 24202469")
 print("Izvēlies kādu no sekojošajiem!:")
@@ -6,14 +8,20 @@ print("A-Apskatīt visas rezervācijas")
 print("B-Veikt pierakstu")
 print("C-Dzēst rezervācijas")
 print("E-Iziet")
+
+# Piešķir mainīgajam lietotāja ievadīto vērtību un uztaisa to par lielo burtu.
 izvele_darb= input('Ievadi izvēlēto burtu: ').upper()
 
+# Izsauc funkciju, lai tā sāk darbību un izvada lietotāja prasīto konsolē.
 jaut.funkcija(izvele_darb)
 
+# Izveidojas fails "rezervacijas.txt", faila pirmajā rindā tiek ierakstīti nosaukumi, zem kuriem uzkrāsies attiecīgie dati.
 while True:
-    try:
-        file = open("rezervacijas.txt", "r")
-    except FileNotFoundError:
-        file = open("rezervacijas.txt", "w+")
-        file.write("#\t\t\t\tVĀRDS UZVĀRDS\t\t\tDATUMS\t\t\tLAIKS\t\t\t\t\tT.NR.\t\t\t\tFRIZIERIS\n")
-    file.close()
+  try:
+    file = open("rezervacijas.txt", "r")
+  except FileNotFoundError:
+    file = open("rezervacijas.txt", "w")
+    file.write("#\t\t\t\tVĀRDS UZVĀRDS\t\t\tDATUMS\t\t\tLAIKS\t\t\t\t\tT.NR.\t\t\t\tFRIZIERIS\n")
+  file.close()
+
+
